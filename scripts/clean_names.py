ff926fb3a421c794302d5d03c37b69c2de1e8c97
David Wallach <davidwallach2@gmail.com>
2018-07-17 13:09:46 -0700

    added python scripts to clean data

---
+++ scripts/clean_names.py
@@ -0,0 +1,37 @@
+import csv
+
+stopwords = ['companies', 'inc.', 'holdings', 'trust', 'corporation', 'incorporated', 'sciences', 
+			'biosciences', 'international', 'plc', 'llc', '(publ)', 'bancorp', 'corp.', 'group', 'airways', 'technology']
+
+tuples = []
+with open('../data/stocks.csv') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	i = 0
+	for row in csv_reader:
+		if i == 0:
+			i += 1
+		else:
+			t = (row[0], row[1].split(',')[0])
+			tuples.append(t)
+
+
+csv_file = open('../data/stocks_cleaned.csv', 'w')
+headers = 'ticker, name\n'
+csv_file.write(headers)
+
+
+for t in tuples:
+
+	name = []
+
+	words = t[1].split(' ')
+	for w in words:
+		if w.lower() in stopwords:
+			continue
+		name.append(w)
+
+	cleaned_name = ' '.join(name)
+
+	if cleaned_name[-4:] == '.com': cleaned_name = cleaned_name[:-4]
+	line = t[0] + ',' + cleaned_name + '\n'
+	csv_file.write(line)
